c93471f0833e80a04ca54d22707f16b971710d51
chloe2407 <21168@stu-snr-21168.chs.ad>
2020-10-22 11:36:15 -0700

    Updates

---
+++ precache-manifest.cd7a177e138401c42fd383a8078ca3c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9a8705b4d38ada419ae37f04711a00",
+    "url": "/luvkie/index.html"
+  },
+  {
+    "revision": "e8ac8eb201f140e37367",
+    "url": "/luvkie/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "d4142a522edd20781b91",
+    "url": "/luvkie/static/js/2.c428e1c0.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/luvkie/static/js/2.c428e1c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8ac8eb201f140e37367",
+    "url": "/luvkie/static/js/main.a0fd3857.chunk.js"
+  },
+  {
+    "revision": "bef06bd4f6d58480e86b",
+    "url": "/luvkie/static/js/runtime-main.b6150bd0.js"
+  },
+  {
+    "revision": "9023d02eefc75f4c6ce177795e620b29",
+    "url": "/luvkie/static/media/doge.9023d02e.jpg"
+  },
+  {
+    "revision": "9af264ff0d8fa0c295ebcc5d0dfebb1d",
+    "url": "/luvkie/static/media/mask.9af264ff.jpeg"
+  },
+  {
+    "revision": "fe4bd3a94acbc2560e58c0638f5b2a4f",
+    "url": "/luvkie/static/media/seed.fe4bd3a9.jpg"
+  }
+]);
