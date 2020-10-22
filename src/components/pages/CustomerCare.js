8d94269f16c376104cc3a3f3a5e94b2bfd90e471
chloe2407 <21168@stu-snr-21168.chs.ad>
2020-10-22 09:30:36 -0700

    test

--- src/components/pages/CustomerCare.js
+++ src/components/pages/CustomerCare.js
@@ -3,7 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const Care = props => {
     return(
-        <div>this is customer care</div>
+        <div>this is customer care
+            <h1>HIII</h1>
+        </div>
+
     );
 };
 
